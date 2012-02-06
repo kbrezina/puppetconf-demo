c4c310bd77fa540acb6af8f6cf81b46afd0194fd
kbrezina <karel.brezina@gmail.com>
2012-02-06 14:36:49 +0100

    ok

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,3 @@
-$newrelic_license = 'e99f4a1d747f84abbe93518cdbc44d9536dbf35c'
 node nd01 {
 	include 'role'
 }
--- roles/production/role/manifests/site.pp
+++ roles/production/role/manifests/site.pp
@@ -1,2 +1,1 @@
-$newrelic_license = 'e99f4a1d747f84abbe93518cdbc44d9536dbf35c'
 include 'role'
