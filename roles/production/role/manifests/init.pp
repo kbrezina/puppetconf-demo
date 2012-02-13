d4d46f0e5ced3ef31ccf424816b3557e1898a7c7
kbrezina <karel.brezina@gmail.com>
2012-02-13 15:34:16 +0100

    ok

--- roles/production/role/manifests/init.pp
+++ roles/production/role/manifests/init.pp
@@ -1,6 +1,8 @@
 class role {
 	include 'apache2' class {
 		'newrelic::server' :
-			collector_host => 'staging-collector.newrelic.com'
+			collector_host => 'staging-collector.newrelic.com',
+			collector_timeout => '',
+			license_key => $::newrelic_license
 	}
 }
