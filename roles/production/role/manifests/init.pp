fac4ba34d2dc205ac2a002ac7ec094a7575a7b90
kbrezina <karel.brezina@gmail.com>
2012-02-13 13:05:28 +0100

    ok

--- roles/production/role/manifests/init.pp
+++ roles/production/role/manifests/init.pp
@@ -1,7 +1,6 @@
 class role {
 	include 'apache2' class {
 		'newrelic::server' :
-			collector_host => 'staging-collector.newrelic.com',
-			collector_timeout => ''
+			collector_host => 'staging-collector.newrelic.com'
 	}
 }
